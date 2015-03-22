6fbe7d2b378ebb4570f6c145cc4e10c66651e836
Brian Ethier <davidbrianethier@gmail.com>
2015-03-22 10:34:24 -0400

    Taking pg out of production, adding some info to cap file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -19,6 +19,7 @@
 
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
+role :web, %w{ec2-user@ec2-54-152-105-174.compute-1.amazonaws.com}
 # role :db,  %w{deploy@example.com}
 
 
