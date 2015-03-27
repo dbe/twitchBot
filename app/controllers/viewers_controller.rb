950d0c858e7d29db170cfc6db6fd12e905fe3ccc
Brian Ethier <davidbrianethier@gmail.com>
2015-03-27 17:38:29 -0400

    Cleaning up the viewers list a bit, taking out access to edit delete etc

--- app/controllers/viewers_controller.rb
+++ app/controllers/viewers_controller.rb
@@ -1,10 +1,10 @@
 class ViewersController < ApplicationController
-  before_action :set_viewer, only: [:show, :edit, :update, :destroy]
+  before_action :set_viewer, only: [:show]
 
   # GET /viewers
   # GET /viewers.json
   def index
-    @viewers = Viewer.all
+    @viewers = Viewer.order('points DESC')
   end
 
   # GET /viewers/1
@@ -13,53 +13,53 @@ class ViewersController < ApplicationController
   end
 
   # GET /viewers/new
-  def new
-    @viewer = Viewer.new
-  end
+  #def new
+  #  @viewer = Viewer.new
+  #end
 
   # GET /viewers/1/edit
-  def edit
-  end
+  #def edit
+  #end
 
   # POST /viewers
   # POST /viewers.json
-  def create
-    @viewer = Viewer.new(viewer_params)
+  #def create
+  #  @viewer = Viewer.new(viewer_params)
 
-    respond_to do |format|
-      if @viewer.save
-        format.html { redirect_to @viewer, notice: 'Viewer was successfully created.' }
-        format.json { render :show, status: :created, location: @viewer }
-      else
-        format.html { render :new }
-        format.json { render json: @viewer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #  respond_to do |format|
+  #    if @viewer.save
+  #      format.html { redirect_to @viewer, notice: 'Viewer was successfully created.' }
+  #      format.json { render :show, status: :created, location: @viewer }
+  #    else
+  #      format.html { render :new }
+  #      format.json { render json: @viewer.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # PATCH/PUT /viewers/1
   # PATCH/PUT /viewers/1.json
-  def update
-    respond_to do |format|
-      if @viewer.update(viewer_params)
-        format.html { redirect_to @viewer, notice: 'Viewer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @viewer }
-      else
-        format.html { render :edit }
-        format.json { render json: @viewer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+  #  respond_to do |format|
+  #    if @viewer.update(viewer_params)
+  #      format.html { redirect_to @viewer, notice: 'Viewer was successfully updated.' }
+  #      format.json { render :show, status: :ok, location: @viewer }
+  #    else
+  #      format.html { render :edit }
+  #      format.json { render json: @viewer.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   # DELETE /viewers/1
   # DELETE /viewers/1.json
-  def destroy
-    @viewer.destroy
-    respond_to do |format|
-      format.html { redirect_to viewers_url, notice: 'Viewer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+  #  @viewer.destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to viewers_url, notice: 'Viewer was successfully destroyed.' }
+  #    format.json { head :no_content }
+  #  end
+  #end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'viewers#index'
-  resources :viewers
+  resources :viewers, only: [:index, :show]
 
 end
