43ffa08bfe0ccaaca5e969d773605330822906f8
Brian Ethier <davidbrianethier@gmail.com>
2015-03-27 17:42:56 -0400

    Setting ip for deploys to the new EIP for EC2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server 'ec2-54-152-105-174.compute-1.amazonaws.com', user: 'ec2-user', roles: %w{app db web}
+server '52.4.18.92', user: 'ec2-user', roles: %w{app db web}
 
 
 
